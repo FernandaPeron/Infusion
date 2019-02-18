2190b9dab6300347761692003fa39f80dfe8d5b5
Fernanda Peron <ferfkp@hotmail.com>
2019-02-17 21:08:35 -0300

    animate menu

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,27 @@
 function Menu(config) {
-    this.nav = (typeof config.container === 'String') ?
-        document.querySelector(config.container) : config.container;
-    this.btn = (typeof config.toggleBtn === 'String') ?
-        document.querySelector(config.toggleBtn) : config.toggleBtn;
-    this.maxWidth = config.widthEnabled || false;
+    this.nav =  document.getElementsByClassName(config.container).item(0);
+    this.btn = document.getElementsByClassName(config.toggleBtn).item(0);
 
     var opened = false;
     var _this = this;
 
+    closeMenu();
 
-    function applyStyleToNav(_style) {
-        Object.keys(_style).forEach(function(stl) {
-            _this.nav.style[stl] = _style[stl];
-        });
+    this.btn.addEventListener('click', openOrClose);
+
+    function openOrClose() {
+        if(!opened){
+            openMenu();
+            return;
+        }
+        closeMenu();
+
     }
 
     function openMenu() {
         var _top = _this.nav.getBoundingClientRect().top + 'px';
         var _style = {
-            maxHeight: 'calc(100vh -' + _top + ')',
+            maxHeight: 'calc(100vh - ' + _top + ')',
             overflow: 'hidden'
         };
 
@@ -26,6 +29,13 @@ function Menu(config) {
 
         opened = true;
     }
+
+    function applyStyleToNav(_style) {
+        Object.keys(_style).forEach(function(stl) {
+            _this.nav.style[stl] = _style[stl];
+        });
+    }
+
 
     function closeMenu() {
         var _style = {
@@ -37,14 +47,4 @@ function Menu(config) {
         opened = false;
     }
 
-    function openOrClose() {
-        if(!opened){
-            openMenu();
-            return;
-        }
-        closeMenu();
-
-    }
-
-    this.btn.addEventListener('click', openOrClose)
 }
